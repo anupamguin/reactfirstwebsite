d3a9c48c8b6a77a59c299ac0dd2adf1424b680fb
anupamguin <programmingboy7585@gmail.com>
2020-08-27 23:18:06 +0530

    Updates

---
+++ precache-manifest.6da7a4579e10b10739d0324ef155359a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82285ec5e371ffbecac43c8834482d04",
+    "url": "/reactfirstwebsite/index.html"
+  },
+  {
+    "revision": "f1d41696773bedc2ce87",
+    "url": "/reactfirstwebsite/static/css/2.86ba9852.chunk.css"
+  },
+  {
+    "revision": "7c5ce208f2294c42dde0",
+    "url": "/reactfirstwebsite/static/css/main.3ab6f0b0.chunk.css"
+  },
+  {
+    "revision": "f1d41696773bedc2ce87",
+    "url": "/reactfirstwebsite/static/js/2.21841fed.chunk.js"
+  },
+  {
+    "revision": "4a9ed9b339a73da435320a5d4728ba66",
+    "url": "/reactfirstwebsite/static/js/2.21841fed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5ce208f2294c42dde0",
+    "url": "/reactfirstwebsite/static/js/main.4836a170.chunk.js"
+  },
+  {
+    "revision": "120abf857f7dca4ef0f3",
+    "url": "/reactfirstwebsite/static/js/runtime-main.935e4a9b.js"
+  },
+  {
+    "revision": "bc2c94426b17db37802441619d8c99a2",
+    "url": "/reactfirstwebsite/static/media/computer.bc2c9442.jpg"
+  },
+  {
+    "revision": "36e44d634c8b9093bcef09c153cda993",
+    "url": "/reactfirstwebsite/static/media/digital.36e44d63.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactfirstwebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactfirstwebsite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactfirstwebsite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactfirstwebsite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactfirstwebsite/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bf167e8251f9cdf209296a948e9752ca",
+    "url": "/reactfirstwebsite/static/media/img2.bf167e82.png"
+  },
+  {
+    "revision": "ef7451c2fc0a2793694040193c9836c4",
+    "url": "/reactfirstwebsite/static/media/laptop.ef7451c2.png"
+  },
+  {
+    "revision": "0fbaf374173d2ac038a4913a1017479a",
+    "url": "/reactfirstwebsite/static/media/software.0fbaf374.jpg"
+  },
+  {
+    "revision": "1e879b88686d064c618db0d3e75981d2",
+    "url": "/reactfirstwebsite/static/media/software_dev.1e879b88.jpg"
+  },
+  {
+    "revision": "3e34384b9db1ce08a1a530e15fa163d3",
+    "url": "/reactfirstwebsite/static/media/web.3e34384b.jpg"
+  }
+]);
